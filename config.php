69b69836ed54cd173b6b086c60587cef888fa944
click2cloud-anshul <68548740+click2cloud-anshul@users.noreply.github.com>
2021-03-03 14:13:18 +0530

    added db config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
+	define('DB_HOST', 'rm-gs5j5l88by3g7738nho.mysql.singapore.rds.aliyuncs.com');
+    define('DB_USER', 'idukan');
+    define('DB_PASSWORD', 'ROOT#123');
     define('DB_DATABASE', 'idukan');
 ?>
