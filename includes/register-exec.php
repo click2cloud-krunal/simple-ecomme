dbcba477e521eee1001872d3909241cdd0b00ff3
click2cloud-krunal <77661681+click2cloud-krunal@users.noreply.github.com>
2021-03-03 14:34:33 +0530

    1
    1

--- includes/register-exec.php
+++ includes/register-exec.php
@@ -12,15 +12,9 @@
 	$errflag = false;
 	
 	//Connect to mysql server
-	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE);
 	if(!$link) {
 		die('Failed to connect to server: ' . mysqli_error());
-	}
-	
-	//Select database
-	$db = mysqli_select_db(DB_DATABASE);
-	if(!$db) {
-		die("Unable to select database");
 	}
 	
 	//Function to sanitize values received from the form. Prevents SQL injection
