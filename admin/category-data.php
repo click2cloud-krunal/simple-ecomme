b531e1915bbefaaf4de2887718f1cfe99354160d
click2cloud-krunal <77661681+click2cloud-krunal@users.noreply.github.com>
2021-03-03 16:57:37 +0530

    Update category-data.php

--- admin/category-data.php
+++ admin/category-data.php
@@ -2,23 +2,20 @@
 if(!isset($_SESSION)) session_start();
 //Include database connection details
 require_once(__DIR__.'/../config.php');
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE);
 if (!$link) {
 	die("Cannot access db.");
 }
 
-$db = mysql_select_db(DB_DATABASE);
-if(!$db) {
-	die("Unable to select database");
-}
+
 $categories;
 //get all the categories
-$res = mysql_query("SELECT count(`tbl_product`.`cat_id`) as `product_count`,`tbl_category`.*
+$res = mysqli_query($link,"SELECT count(`tbl_product`.`cat_id`) as `product_count`,`tbl_category`.*
 					FROM `tbl_category`
 					LEFT JOIN `tbl_product`
 					ON `tbl_product`.`cat_id`=`tbl_category`.`cat_id`
 					GROUP BY `tbl_category`.`cat_id`;");
-while ($row = mysql_fetch_object($res)) {
+while ($row = mysqli_fetch_object($res)) {
 	$categories[] = $row;
 }
 //handle new category request
@@ -41,7 +38,7 @@ if(is_array($_POST) && count($_POST) > 0) {
 	//Create INSERT query
 	$qry = "INSERT INTO `tbl_category` ( `cat_name`, `cat_description`)
 			VALUES ('".$catname."', '".$catdesc."');";
-	$result = @mysql_query($qry);
+	$result = mysqli_query($link,$qry);
 	//Check whether the query was successful or not
 	if($result) {
 		$_SESSION['MSGS'] = array('<strong>Wola!</strong> Changes were successful.');
