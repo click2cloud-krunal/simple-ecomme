dce26d8a546dd7cfef0c9a1c8d6de28b77bc61c7
click2cloud-krunal <77661681+click2cloud-krunal@users.noreply.github.com>
2021-03-05 10:33:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'rm-1ud33xyf567we2x0n0o.mysql.rds.aliyuncs.com');
+	define('DB_HOST', 'rm-1uda38y50f2j858c2jo.mysql.rds.aliyuncs.com');
     define('DB_USER', 'idukan');
     define('DB_PASSWORD', 'ROOT#123');
     define('DB_DATABASE', 'idukan');
